e9778f5d7476581e340dadd5f4ac33b2227c9054
Curtis <curtis@lassam.net>
2022-12-22 13:06:30 -0800

    improved light management with transparent popovers

--- game/tools/transparent_parent_area.gd
+++ game/tools/transparent_parent_area.gd
@@ -19,11 +19,41 @@ func _ready():
 	
 	child.body_entered.connect(_on_body_entered)
 	child.body_exited.connect(_on_body_exited)
+	
+	# for good measure, make sure that we're in the "non-transparent" state
+
+func transparentify_parent():
+	get_parent().modulate.a = 0.6
+
+func hide_visible_children():
+	for child in get_children():
+		if "visible" in child.name or "Visible" in child.name:
+			child.hide()
+		if "transparent" in child.name or "Transparent" in child.name:
+			child.show()
+
+func opaquify_parent():
+	get_parent().modulate.a = 1.0
+
+func show_visible_children():
+	for child in get_children():
+		if "visible" in child.name or "Visible" in child.name:
+			child.show()
+		if "transparent" in child.name or "Transparent" in child.name:
+			child.hide()
+
+func toggle_on():
+	transparentify_parent()
+	hide_visible_children()
+
+func toggle_off():
+	opaquify_parent()
+	show_visible_children()
 
 func _on_body_entered(body):
 	if body is CharacterBody2D:	
-		get_parent().modulate.a = 0.6
+		toggle_on()
 
 func _on_body_exited(body):
 	if body is CharacterBody2D:	
-		get_parent().modulate.a = 1.0
+		toggle_off()

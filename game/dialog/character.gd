6d6a73283459a6275d58e706ee34ff67854ad9c0
Curtis <curtis@lassam.net>
2023-01-16 11:12:24 -0800

    colorful

--- game/dialog/character.gd
+++ game/dialog/character.gd
@@ -10,10 +10,16 @@ func _process(delta):
 	pass
 
 func setChar(c):
-	$Character.text = c
 	$Character/ColorAnimation.play("RESET")
 	$Character/ScaleAnimation.play("RESET")
 	$Character/PositionAnimation.play("RESET")
+	$Character.text = c
+
+func clear():
+	$Character/ColorAnimation.play("RESET")
+	$Character/ScaleAnimation.play("RESET")
+	$Character/PositionAnimation.play("RESET")
+	$Character.modulate = Color("#ffffff")
 
 func wobble():
 	$Character/PositionAnimation.play("wobble")
@@ -32,3 +38,52 @@ func kachunk():
 
 func rainbow():
 	$Character/ColorAnimation.play("rainbow")
+
+func zariel():
+	$Character/ColorAnimation.play("zariel")
+	$Character.modulate = Color("#01fff8")
+
+func oth():
+	$Character/ColorAnimation.play("oth")
+	$Character.modulate = Color("#01ff7f")
+
+func cystam():
+	$Character/ColorAnimation.play("cystam")
+	$Character.modulate = Color("#03f80c")
+
+func kiro():
+	$Character/ColorAnimation.play("kiro")
+	$Character.modulate = Color("#82fe00")
+
+func audient():
+	$Character/ColorAnimation.play("audient")
+	$Character.modulate = Color("#f3ff00")
+
+func mersenne():
+	$Character/ColorAnimation.play("mersenne")
+	$Character.modulate = Color("#ff9b00")
+
+func world():
+	$Character/ColorAnimation.play("world")
+	$Character.modulate = Color("#ff0000")
+
+func path():
+	$Character/ColorAnimation.play("path")
+	$Character.modulate = Color("#ff007e")
+
+func curopal():
+	$Character/ColorAnimation.play("curopal")
+	$Character.modulate = Color("#ff00e6")
+
+func blit():
+	$Character/ColorAnimation.play("blit")
+	$Character.modulate = Color("#8500ff")
+
+func stacks():
+	$Character/ColorAnimation.play("stacks")
+	$Character.modulate = Color("#1603ff")
+
+func misk():
+	$Character/ColorAnimation.play("misk")
+	$Character.modulate = Color("#0366ff")
+
--- game/dialog/dialogbox.gd
+++ game/dialog/dialogbox.gd
@@ -5,23 +5,47 @@ var default_speed = 0.07
 var pause_time = 1.0
 var NEWLINE_CODE = "%n"
 
-# command language:
+# Command Language:
 # everything between %% is a command
-# #w# (toggle wiggle)
-# %wr% (toggle wiggle, rainbow)
+#
+# ## Toggles
+# r - rainbow
+# k - kachunk
+# w - wiggle
+# W - WIGGLE
+# j - jitter
+# J - JITTER
+# f - fast
+# s - slow
+# 
+# ## Colors
+# 1 - oth (teal)
+#
+# ## Events
+# p - pause for pause_time seconds
+# n - newline
+# 
+# e.g.: %Wrs% (slow WIGGLE rainbow)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#await say("%wr%ROYAL RAINBOW beeeoooooowwwww")
-	await say("this %j%coffee%j% is so %ws%smoooooth%ws% %p% %n%this %J%COFFEE%J% is so %W%SMOOOOOTH%W%")
+	await say("%0%zariel%0% %1%oth%1% %2%cystam%2% %3%kiro%3% %4%audient%4% %5%mersenne%5% %6%world%6% %7%path%7% %8%curopal%8% %9%blit%9% %a%stacks%a% %b%misk%b% ")
+	#await say("this %j6%coffee%j6% is so %ws6%smoooooth%ws6% %p% %n%this %J6%COFFEE%J6% is so %W6%SMOOOOOTH%W6%")
 	#await say("then the keyboard was like %k%clacka clacka clacka clacka %p% ker-ching!%k% %p%")
 	#await say("and then he said %W%WHOOOAAAAH HAH%W%!!! %p%")
 	#await say("normal %f%fast fast fast%f% %s%slow slow slow%s% and then %p%stop")
 	#await say("the radio this morning promised %w%ten tonnes of cheese%w% to the highest bidder")
 	#await say("charming superflous valorous %w%chimichangas%w% rustle gloriously in all weather conditions, regardless")
 
+
+
 # poison, fire, ice words
 # todo: colors
+# todo: letters swing
+# todo: letters vanish in
+# todo: angle ( a little bit different every time ? )
+# todo: the whole dialog box gets whacked and all of the letters come a little loose
 # todo: little extra time after a comma or period
 # todo: "angry", "embarrassed", "sad", "frosty", "clever", "smug", "goofy", 
 # todo: multi-page "say" commands for long sentences
@@ -31,6 +55,7 @@ func _ready():
 # todo: appear animation
 # todo: profile pic
 # todo: changeable background
+# todo: backspace
 # todo: "character set": characters might talk at different speed or with different inflections and we want to know how we can reflect that in the text box
 
 
@@ -89,7 +114,20 @@ func _display(string):
 	var jitter_flag = false
 	var jutter_flag = false
 	var kachunk_flag = false
+	#colors
 	var rainbow_flag = false
+	var zariel_flag = false; #teal
+	var oth_flag = false #turquoise
+	var cystam_flag = false #green
+	var kiro_flag = false #lime
+	var audient_flag = false #lemon
+	var mersenne_flag = false #orange
+	var world_flag = false #red
+	var path_flag = false #pink
+	var curopal_flag = false #magenta
+	var blit_flag = false #purple
+	var stacks_flag = false #blue
+	var misk_flag = false #sky
 	var letters = letter_elements()
 	
 	for character in string:
@@ -97,26 +135,54 @@ func _display(string):
 			print ("Warning! Phrase exceeds maximum size: ", string)
 			pass
 		elif command_mode:
-			if String(character) == "w":
+			if character == "w":
 				wiggle_flag = !wiggle_flag
-			if String(character) == "W":
+			elif character == "W":
 				wobble_flag = !wobble_flag
-			if String(character) == "j":
+			elif character == "j":
 				jitter_flag = !jitter_flag
-			if String(character) == "J":
+			elif character == "J":
 				jutter_flag = !jutter_flag
-			if String(character) == "k":
+			elif character == "k":
 				kachunk_flag = !kachunk_flag
-			if String(character) == "r":
+			elif character == "r":
 				rainbow_flag = !rainbow_flag
-			if String(character) == "f":
+			elif character == "f":
 				fast_flag = !fast_flag
-			if String(character) == "s":
+			elif character == "s":
 				slow_flag = !slow_flag
-			if String(character) == "p":
+			elif character == "0":
+				print("doof")
+				zariel_flag = !zariel_flag
+				print("zflag: ", zariel_flag)
+			elif character == "1":
+				oth_flag = !oth_flag
+			elif character == "2":
+				cystam_flag = !cystam_flag
+			elif character == "3":
+				kiro_flag = !kiro_flag
+			elif character == "4":
+				audient_flag = !audient_flag
+			elif character == "5":
+				mersenne_flag = !mersenne_flag
+			elif character == "6":
+				world_flag = !world_flag
+			elif character == "7":
+				path_flag = !path_flag
+			elif character == "8":
+				curopal_flag = !curopal_flag
+			elif character == "9":
+				blit_flag = !blit_flag
+			elif character == "a":
+				stacks_flag = !stacks_flag
+			elif character == "b":
+				misk_flag = !misk_flag
+			elif character == "p":
 				await get_tree().create_timer(pause_time).timeout
-			if String(character) == "%":
+			elif character == "%":
 				command_mode = false
+			else:
+				print("Unknown control character: ", character)
 		elif String(character) == "%":
 			command_mode = true
 		else:
@@ -134,6 +200,31 @@ func _display(string):
 				child.kachunk()
 			if rainbow_flag:
 				child.rainbow()
+			if zariel_flag:
+				print("boof")
+				child.zariel()
+			if oth_flag:
+				child.oth()
+			if cystam_flag:
+				child.cystam()
+			if kiro_flag:
+				child.kiro()
+			if audient_flag:
+				child.audient()
+			if mersenne_flag:
+				child.mersenne()
+			if world_flag:
+				child.world()
+			if path_flag:
+				child.path()
+			if curopal_flag:
+				child.curopal()
+			if blit_flag:
+				child.blit()
+			if stacks_flag:
+				child.stacks()
+			if misk_flag:
+				child.misk()
 			if character != " ":
 				var speed = default_speed
 				if fast_flag:
